6ed72be33a80dcacbe69fa4edde8ea759807b4b9
Pelegrine365 <pelegrine365@icloud.com>
2025-07-15 22:51:43 +0300

    refactor: restructure all components

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,20 +1,20 @@
 import { Component } from 'react';
 
-import { getPokemonsBySearch } from '../services/pokemonService';
+import { getPokemonsBySearch } from '../../services/pokemonService';
 import {
   clearSearchValue,
   getSavedSearchValue,
   saveSearchValue,
-} from '../api/searchStorage';
+} from '../../api/searchStorage';
 
-import SearchBar from '../components/SearchBar';
-import CardList from '../components/CardList';
-import Spinner from '../components/Spinner';
-import ErrorMessage from '../components/ErrorMessage';
-import ErrorBoundary from '../components/ErrorBoundary';
-import BrokenComponent from '../components/BrokenComponent';
+import SearchBar from '../SearchBar/SearchBar';
+import CardList from '../CardList/CardList';
+import Spinner from '../Spinner/Spinner';
+import ErrorMessage from '../ErrorMessage';
+import ErrorBoundary from '../ErrorBoundary';
+import BrokenComponent from '../BrokenComponent';
 
-import type { Pokemon } from '../types';
+import type { Pokemon } from '../../types';
 
 import './index.css';
 

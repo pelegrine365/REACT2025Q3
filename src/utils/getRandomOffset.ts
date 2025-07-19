da91e33ac0ad094cfc53ec4279870f33804a9b3b
Pelegrine365 <pelegrine365@icloud.com>
2025-07-19 23:04:52 +0300

    fix: update getRandomOffset function

--- src/utils/getRandomOffset.ts
+++ src/utils/getRandomOffset.ts
@@ -2,6 +2,10 @@ export function getRandomOffset(
   totalCount: number,
   requestCount: number
 ): number {
+  if (requestCount <= 0) {
+    throw new Error('requestCount must be greater than 0');
+  }
+
   const maxOffset = totalCount - requestCount;
   if (maxOffset <= 0) return 0;
 

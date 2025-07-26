6be8d3ed8120d954197cd212e1a2309c3ac43ac5
Pelegrine365 <pelegrine365@icloud.com>
2025-07-26 21:44:19 +0300

    refactor: change POKEMON_TOTAL_COUNT

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,5 +1,5 @@
 export const SEARCH_VALUE_KEY = 'searchValue';
 export const POKEMONS_PER_REQUEST = 10;
-export const POKEMON_TOTAL_COUNT = 1500;
+export const POKEMON_TOTAL_COUNT = 500;
 
 export const AUTHOR_GITHUB_NAME = 'pelegrine365';
